d2badf928f55257730e9d346ce843655ccd1c55b
4AMLA <bingoleex@gmail.com>
2016-07-07 18:04:33 +0800

    add delay function

--- Serial/serial_demo.py
+++ Serial/serial_demo.py
@@ -3,6 +3,7 @@ from binascii import hexlify, unhexlify
 from time import sleep
 import json
 import os
+import time
 
 class SerialAPI(object):
 
@@ -23,22 +24,33 @@ class SerialAPI(object):
 		data = self.hex_to_binary(data)
 		self.ser.write(data)
 
-	def recieve(self):
+	def recieve(self,timeout=15):
+		start = int(time.time())
 		while True:
 			data = self.ser.read(1)
 			if data == '':
-			    continue
+				end = int(time.time())
+				run_time = end - start
+				if run_time>timeout:
+					break
+				else:
+					continue
 			while True:
 			    n = self.ser.inWaiting()
+
 			    if n > 0:
 			        data = "%s%s" % (data, self.ser.read(n))
-			        sleep(0.02) # data is this interval will be merged
+			        sleep(0.02) #  add a internal in case can't recieve data
 			    else:
 			        quit = True
 			        break
+
 			if quit:
 			    break
-		return self.binary_to_hex(data)
+		if run_time>timeout:
+			return "Recieve data timeout!!!"
+		else:
+			return self.binary_to_hex(data)
 
 	def hex_to_binary(self, data):
 		return ''.join([unhexlify(data[i:i+2]) for i in xrange(0, len(data), 2)])
@@ -48,3 +60,17 @@ class SerialAPI(object):
 
 	def close(self):
 		self.ser.close()
+
+	def delay_send(self, command, sleep_time):
+		print "send command:",command
+		print "delay time:",sleep_time
+		self.send(command)
+		sleep(int(sleep_time))
+
+# example to run:
+
+# s = SerialAPI()
+# s.open()
+# s.send('0200310000009303')
+# print s.recieve()
+# s.close()
