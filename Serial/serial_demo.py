745b015656f5301f3828181fd2c0f41725f221ef
4AMLA <bingoleex@gmail.com>
2016-07-07 16:36:45 +0800

    inherts

--- Serial/serial_demo.py
+++ Serial/serial_demo.py
@@ -1,34 +1,50 @@
 import serial
 from binascii import hexlify, unhexlify
 from time import sleep
+import json
+import os
 
-#setting
-port = "COM4"
-baudrate=9600
-command = '0200310000009303'
+class SerialAPI(object):
 
+	def __init__(self):
+		if os.path.isfile('config.json'):
+			with open('config.json','r') as f:
+				self.elements = json.load(f)
+			self.port = self.elements['port']
+			self.baudrate = self.elements['baudrate']
+		else:
+			self.port = "COM1"
+			self.baudrate = "9600"		
 
-#Return the binary data represented by the hexadecimal string 
-toHex = lambda hex_data: ''.join([unhexlify(hex_data[i:i+2]) for i in xrange(0, len(hex_data), 2)])
+	def open(self):
+		self.ser = serial.Serial(self.port, self.baudrate,timeout=0)
 
-#Return the hexadecimal representation of the binary data
-toVisualHex = lambda hex_data: ' '.join([hexlify(c) for c in hex_data]).upper()
+	def send(self, data):
+		data = self.hex_to_binary(data)
+		self.ser.write(data)
 
-with serial.Serial(port,baudrate,timeout=0) as ser:
-	ser.write(toHex(command))
-	while True:
-		data = ser.read(1)
-		if data == '':
-		    continue
+	def recieve(self):
 		while True:
-		    n = ser.inWaiting()
-		    if n > 0:
-		        data = "%s%s" % (data, ser.read(n))
-		        sleep(0.02) # data is this interval will be merged
-		    else:
-		        quit = True
-		        break
-		if quit:
-		    break
+			data = self.ser.read(1)
+			if data == '':
+			    continue
+			while True:
+			    n = self.ser.inWaiting()
+			    if n > 0:
+			        data = "%s%s" % (data, self.ser.read(n))
+			        sleep(0.02) # data is this interval will be merged
+			    else:
+			        quit = True
+			        break
+			if quit:
+			    break
+		return self.binary_to_hex(data)
 
-print toVisualHex(data)
+	def hex_to_binary(self, data):
+		return ''.join([unhexlify(data[i:i+2]) for i in xrange(0, len(data), 2)])
+
+	def binary_to_hex(self,data):	
+		return ' '.join([hexlify(c) for c in data]).upper()
+
+	def close(self):
+		self.ser.close()
