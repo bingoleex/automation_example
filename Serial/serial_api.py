a952ffb242edb2336243c9f4fcb70919c0c9e0e5
4AMLA <bingoleex@gmail.com>
2016-07-13 18:00:48 +0800

    support python 3.x

--- Serial/serial_api.py
+++ Serial/serial_api.py
@@ -21,11 +21,12 @@ class SerialAPI(object):
 		self.ser = serial.Serial(self.port, self.baudrate,timeout=0)
 
 	def send(self, data):
-		data = self.hex_to_binary(data)
+		data = self.hex_to_binary(self.remove_space(data))
 		self.ser.write(data)
 
 	def recieve(self,timeout=15):
 		start = int(time.time())
+		run_time = 0
 		while True:
 			data = self.ser.read(1)
 			if data == '':
@@ -40,7 +41,7 @@ class SerialAPI(object):
 
 			    if n > 0:
 			        data = "%s%s" % (data, self.ser.read(n))
-			        sleep(0.02) #  add a internal in case can't recieve data
+			        sleep(0.5) #  add a internal in case can't recieve data
 			    else:
 			        quit = True
 			        break
@@ -53,17 +54,33 @@ class SerialAPI(object):
 			return self.binary_to_hex(data)
 
 	def hex_to_binary(self, data):
+		'''
+		for Python 3：
+		return b''.join([unhexlify(data[i:i+2]) for i in range(0, len(data), 2)])
+		'''
 		return ''.join([unhexlify(data[i:i+2]) for i in xrange(0, len(data), 2)])
 
-	def binary_to_hex(self,data):	
+	def binary_to_hex(self,data):
+		'''
+		for Python 3:
+		fillter = "bx\'\\"
+		hex_data =  [c for c in data if str(c) not in fillter]
+		data = [hexlify(bytearray(c,'utf-8')).decode() for c in hex_data[2:-2]]
+		data.insert(0, '02')
+		data.insert(len(data),'03')
+		return ' '.join([byte for byte in data])
+		'''
 		return ' '.join([hexlify(c) for c in data]).upper()
+
+	def remove_space(self, data):
+		return data.replace(" ","")
 
 	def close(self):
 		self.ser.close()
 
 	def delay_send(self, command, sleep_time):
-		print "send command:",command
-		print "delay time:",sleep_time
+		# print "send command:",command
+		# print "delay time:",sleep_time
 		self.send(command)
 		sleep(int(sleep_time))
 
